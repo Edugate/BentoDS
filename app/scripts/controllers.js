3bffba22c20ae825fff75495cb8efb7d57bfa3d1
janul <janusz.ulanowski@heanet.ie>
2016-01-26 16:46:03 +0000

    minor fixes

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,15 +1,22 @@
 var dsControllers = angular.module('dsControllers', []);
 
-dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$location', '$base64', '$http', 'GeolocationService', 'DistanceCalculatorService', 'gMapper', '$uibModal', function (config, $scope, ipCookie,  $location, $base64, $http, geolocation, DistanceCalculatorService, gMapper, $uibModal) {
-  "use strict";
-
+dsControllers.controller('ProviderListCtrl', ['config', '$scope', 'ipCookie', '$location', '$base64', '$http', 'GeolocationService', 'DistanceCalculatorService', 'gMapper', '$uibModal', function (config, $scope, ipCookie, $location, $base64, $http, geolocation, DistanceCalculatorService, gMapper, $uibModal) {
+    "use strict";
+    $scope.errormessage = null;
     var params = $location.search();
+    if (params.entityID === null || params.entityID === undefined || params.entityID === '') {
+        window.console.log('params.entityID does not exist');
+        $scope.loadingList = false;
+        $scope.errormessage = 'Requester\'s entityID has not been provided';
+        return false;
+    }
+
+ window.console.log('params.entityID  is:"'+params.entityID+'"');
     var pentityid = params.entityID;
     var preturn = params.return;
     var returnidparam = params.returnIDParam;
     $scope.returnidparam = params.returnIDParam;
-    if($scope.returnidparam === undefined || $scope.returnidparam === null || $scope.returnidparam ==='')
-    {
+    if ($scope.returnidparam === undefined || $scope.returnidparam === null || $scope.returnidparam === '') {
         $scope.returnidparam = 'entityID';
     }
     $scope.returnparam = params.return;
@@ -41,7 +48,7 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
 
     $scope.SP = null;
 
-    if(config.srcRequesterInfo !== '') {
+    if (config.srcRequesterInfo !== '') {
         var urlSrcSP = config.srcRequesterInfo + encodedEntityid + '';
         $http.get(urlSrcSP).success(function (data) {
             if (data) {
@@ -67,40 +74,40 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
     var urlSrcFallback = config.srcIdPListFallback;
     $scope.loadingList = true;
     $http.get(urlSrc).then(function (response) {
-        $scope.mdata = response.data;
-        window.console.log('DOWNLOADED');
-        if (angular.isArray($scope.mdata)) {
+            $scope.mdata = response.data;
+            window.console.log('DOWNLOADED');
+            if (angular.isArray($scope.mdata)) {
 
-          if (($scope.position !== null) || ($scope.lastSelected !== null)) {
-            angular.forEach($scope.mdata, function (value, key) {
-              value.pr = 0;
+                if (($scope.position !== null) || ($scope.lastSelected !== null)) {
+                    angular.forEach($scope.mdata, function (value, key) {
+                        value.pr = 0;
 
-              if ($scope.position !== null && value.geo !== undefined) {
-                value.distance = DistanceCalculatorService.calcCrow($scope.position.coords, value.geo);
-              }
-              if (value.entityID === $scope.prevlastSelected) {
-                value.pr = value.pr - 100;
-              }
-              else if (value.entityID === $scope.lastSelected) {
-                window.console.log('ENTITYID ' + value.entityID);
-                value.pr = value.pr - 200;
-              }
+                        if ($scope.position !== null && value.geo !== undefined) {
+                            value.distance = DistanceCalculatorService.calcCrow($scope.position.coords, value.geo);
+                        }
+                        if (value.entityID === $scope.prevlastSelected) {
+                            value.pr = value.pr - 100;
+                        }
+                        else if (value.entityID === $scope.lastSelected) {
+                            window.console.log('ENTITYID ' + value.entityID);
+                            value.pr = value.pr - 200;
+                        }
 
-              window.console.log('PR: ' + value.pr);
+                        window.console.log('PR: ' + value.pr);
 
-            });
-          }
-        }
+                    });
+                }
+            }
 
-        $scope.loadingList = false;
-        $scope.idpList = response.data;
+            $scope.loadingList = false;
+            $scope.idpList = response.data;
 
         },
         function () {
             window.console.log('json errr');
             $scope.loadingList = false;
 
-            if(urlSrcFallback !== undefined && urlSrcFallback !== null) {
+            if (urlSrcFallback !== undefined && urlSrcFallback !== null) {
                 $scope.errormessage = 'Could not load the list of Identity Providers. Trying to load backup...';
                 $http.get(urlSrcFallback).then(function (response) {
                     $scope.mdata = response.data;
@@ -113,16 +120,15 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
                                 if ($scope.position !== null && value.geo !== undefined) {
                                     value.distance = DistanceCalculatorService.calcCrow($scope.position.coords, value.geo);
                                 }
-                                if(value.entityID === $scope.prevlastSelected)
-                                {
+                                if (value.entityID === $scope.prevlastSelected) {
                                     value.pr = value.pr - 100;
                                 }
-                                else if(value.entityID === $scope.lastSelected) {
+                                else if (value.entityID === $scope.lastSelected) {
                                     window.console.log('ENTITYID ' + value.entityID);
                                     value.pr = value.pr - 200;
                                 }
 
-                                window.console.log('PR: '+value.pr);
+                                window.console.log('PR: ' + value.pr);
 
                             });
                         }
@@ -131,19 +137,17 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
                     $scope.idpList = response.data;
                     $scope.errormessage = null;
                     window.console.log('D  SUCCES');
-                }, function(){
+                }, function () {
                     window.console.log('S  FAIL');
                 });
 
             }
-            else
-            {
+            else {
                 $scope.errormessage = 'Could not load the list of Identity Providers';
             }
 
 
         }
-
     );
 
     $scope.$watch("position", function (newValue, oldValue) {
@@ -160,12 +164,11 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
 
     });
 
-    $scope.updatelast = function(v)
-    {
+    $scope.updatelast = function (v) {
 
-        window.console.log(' previus select'+ $scope.lastSelected);
-        ipCookie('dsprevlastsel',$scope.lastSelected, { expires: 21 });
-        ipCookie('dslastsel', v, { expires: 21 });
+        window.console.log(' previus select' + $scope.lastSelected);
+        ipCookie('dsprevlastsel', $scope.lastSelected, {expires: 21});
+        ipCookie('dslastsel', v, {expires: 21});
     };
     $scope.showOnMap = function (v) {
         window.console.log('show on map clicked ' + v.lat + ', ' + v.lon);
@@ -174,7 +177,6 @@ dsControllers.controller('ProviderListCtrl', ['config','$scope', 'ipCookie', '$l
     };
 
     $scope.showModal = function (a, b, c) {
-
 
 
         $scope.opts = {
